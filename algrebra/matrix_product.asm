2819bbca3276285d713bc2e396c89bcf69ec1b40
PtichapronRouge <benjamin.mouscadet@supelec.fr>
2020-05-02 14:37:19 +0200

    FIX: started rewriting MatrixProduct_

--- algrebra/matrix_product.asm
+++ algrebra/matrix_product.asm
@@ -1,58 +1,70 @@
     global MatrixProduct_
     section .text
 
-; C = A*B
 MatrixProduct_:
-    cmp rdi, 0
-    jle InvalidSize
+    ; prolog
+    push r13
+    push r14
+    push r15
 
-    mov r8, rdi ; row iterator
+    ;; check n size
+    ;cmp rdi, 0
+    ;jle InvalidSize
 
-    xor r13, r13 ; C iterator
-    mov r14, rdi
+    ; i = 0
+    xor r8, r8
 
 RowIter:
-    ; k = 0
-    mov r10, rdi
-    mov r12, rdx ; B iterator
-
-VectIter:
     ; j = 0
-    mov r9, rdi
-    mov r13, r14
+    xor r9, r9
 
 ColIter:
-    ; r13 = j + i*n = n + (*n) - (n-j)
-    sub r13, r9
+    ; k = 0
+    xor r10, r10
 
-    ; C[n*i+j] += A[n*i+k]*B[n*k+j]
-    mov  eax, [rsi]
-    imul eax, [r12]
-    add  [rcx + r13*4], eax
+    ; r15 = n*i + j
+    mov r15, r8
+    imul r15, rdi
+    add r15, r9
 
-    ; j++
-    add r12, 4
+VectIter:
+    ; r13 = n*i + k
+    mov r13, r8
+    imul r13, rdi
+    add r13, r10
 
-    ; if j < n, repeat
-    dec r9
-    jnz ColIter
+    ; r14 = n*k + j
+    mov r14, r10
+    imul r14, rdi
+    add r14, r9
+
+    ; eax = A[i,k]*B[k,j]
+    mov eax, [rsi + r13*4]
+    imul eax, [rdx + r14*4]
+    ; C[i,j] += eax
+    add [rcx + r15*4], eax
 
     ; k++
-    add rsi, 4
-    ; if k < n, repeat
-    dec r10
-    jnz VectIter
+    inc r10
+    ; k < n ?
+    cmp r10, rdi
+    jl VectIter
 
-    add r14, rdi
-    dec r8
-    jnz RowIter
+    ; j++
+    inc r9
+    ; j < n ?
+    cmp r9, rdi
+    jl ColIter
 
-    xor eax, eax
-    jmp End
-
-InvalidSize:
-    mov eax, 1
+    ; i++
+    inc r8
+    ; i < n ?
+    cmp r8, rdi
+    jl RowIter
 
 End:
+    xor eax, eax
+    pop r15
+    pop r14
+    pop r13
     ret
-
