8aea1b43dad1043b865ec7baeb0db6bb282ee6a4
PtichapronRouge <benjamin.mouscadet@supelec.fr>
2020-05-02 16:02:16 +0200

    ENH: updated tests

--- algrebra/algebra_test.hpp
+++ algrebra/algebra_test.hpp
@@ -1,39 +1,76 @@
 #include <cxxtest/TestSuite.h>
 
-#include <vector>
-#include <iostream>
-
 extern "C" int MatrixProduct_(int n, int* m1, int* m2, int* res);
 extern "C" int NaiveMatrixProduct_(int n, int* A, int* B, int* C);
+
+void init_matrices(int n, int* A, int* B)
+{
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            A[n*i+j] = i+j;
+            B[n*i+j] = i+j;
+        }
+    }
+}
+
+void erase_matrices(int n, int* C)
+{
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            C[n*i+j] = 0;
+        }
+    }
+}
+
+void MatrixProductCpp(int n, int* A, int* B, int* res)
+{
+    // C function to compute the matrix product
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < n; k++) {
+            for (int j = 0; j < n; j++) {
+                res[n*i+j] += A[n*i+k] * B[n*k+j];
+            }
+        }
+    }
+}
 
 class AlgebraTest : public CxxTest::TestSuite
 {
     public:
         void testNaiveIntegerMatrixProd(void)
         {
-            const int n(5);
+            const int n(20);
             int A[n*n],
                 B[n*n],
                 c_res[n*n],
                 asm_res[n*n];
-            for (int i = 0; i < n; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    A[n*i+j] = i+j;
-                    B[n*i+j] = i+j;
-                    c_res[n*i+j] = 0;
-                    asm_res[n*i+j] = 0;
-                }
-            }
-            // C function to compute the matrix product
-            for (size_t i = 0; i < n; i++) {
-                for (size_t k = 0; k < n; k++) {
-                    for (size_t j = 0; j < n; j++) {
-                        c_res[n*i+j] += A[n*i+k] * B[n*k+j];
-                    }
-                }
-            }
+
+            init_matrices(n, A, B);
+
+            erase_matrices(n, c_res);
+            MatrixProductCpp(n, A, B, c_res);
             // Assembly function to compute the matrix product
-            NaiveMatrixProduct_(n,A,B,asm_res);
+            erase_matrices(n, asm_res);
+            NaiveMatrixProduct_(n, A, B, asm_res);
+            // asm_res & c_res must be equal
+            TS_ASSERT_SAME_DATA(asm_res, c_res, n*n);
+        }
+
+        void testIntegerMatrixProd(void)
+        {
+            const int n(20);
+            int A[n*n],
+                B[n*n],
+                c_res[n*n],
+                asm_res[n*n];
+
+            init_matrices(n, A, B);
+
+            erase_matrices(n, c_res);
+            MatrixProductCpp(n, A, B, c_res);
+            // Assembly function to compute the matrix product
+            erase_matrices(n, asm_res);
+            MatrixProduct_(n, A, B, asm_res);
             // asm_res & c_res must be equal
             TS_ASSERT_SAME_DATA(asm_res, c_res, n*n);
         }
